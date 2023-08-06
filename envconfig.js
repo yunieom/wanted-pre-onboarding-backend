86ec9f4a60d9a22e5b8b41d60388405cf5d51639
yunieom <yunzoo0915@gmail.com>
2023-08-06 18:55:46 +0900

    Docs: envconfig 정리

--- envconfig.js
+++ envconfig.js
@@ -1,11 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    PORT: process.env.PORT,
-    DB_HOST: process.env.DB_HOST,
-    DB_USER: process.env.DB_NAME,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_DATABASE: process.env.DB_DATABASE,
-    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
-    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET
+  PORT: process.env.PORT,
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_NAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_DATABASE: process.env.DB_DATABASE,
 };
